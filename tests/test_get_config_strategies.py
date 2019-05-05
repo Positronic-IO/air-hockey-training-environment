22cc3eff94fc4a9b42d963c4c393dca8ae971598
Tony Hammack <hammack.tony@gmail.com>
2019-05-04 23:04:16 -0500

    class test

--- tests/test_get_config_strategies.py
+++ tests/test_get_config_strategies.py
@@ -3,16 +3,18 @@ import pytest
 
 from utils import get_config_strategy
 
-def test_human_strategy():
-    """ Test human strategy input """
-    assert get_config_strategy("human") == "human"
 
-def test_rl_strategy():
-    """ Test a rl strategy """
-    assert isinstance(get_config_strategy("c51"), dict)
+class TestStrategyConfig:
+    def setup(self):
+        pass
 
-def test_undefined_strategy():
-    """ Test a strategy not defined """
+    def test_rl_strategy(self):
+        """ Test a rl strategy """
+        assert isinstance(get_config_strategy("c51"), dict)
 
-    with pytest.raises(KeyError):
-        get_config_strategy("hi")
+    def test_undefined_strategy(self):
+        """ Test a strategy not defined """
+
+        with pytest.raises(KeyError):
+            get_config_strategy("hi")
+
