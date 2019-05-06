6660c46027201b25e97a65d99ca60d6d8ed97ea8
Tony Hammack <hammack.tony@gmail.com>
2019-05-06 12:18:36 -0500

    Update tests to for refactoring

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -14,60 +14,43 @@ from rl.Strategy import Strategy
 class TestStrategy:
     def setup(self):
         self.env = AirHockey()
-        self.tbl = TensorBoardLogger(log_dir="logs/test")
+        # self.tbl = TensorBoardLogger(log_dir="logs/test")
+        self.train = True
+        self.capacity = 4
 
     def test_human_agent(self):
         """ Test to see if created agent is human """
 
-        strategy = Strategy().make(
-            name="human", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "human")
         assert isinstance(strategy, Agent)
 
     def test_ddqn_agent(self):
         """ Test to see if created agent is ddqn """
 
-        strategy = Strategy().make(
-            name="ddqn", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "ddqn", self.capacity, self.train)
         assert isinstance(strategy, DDQN)
 
     def test_dqn_agent(self):
         """ Test to see if created agent is dqn """
 
-        strategy = Strategy().make(
-            name="dqn", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "dqn", self.capacity, self.train)
         assert isinstance(strategy, DQN)
 
     def test_dueling_agent(self):
         """ Test to see if created agent is dueling """
 
-        strategy = Strategy().make(
-            name="dueling", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "dueling", self.capacity, self.train)
         assert isinstance(strategy, DuelingDDQN)
 
     def test_c51_agent(self):
         """ Test to see if created agent is c51 """
 
-        strategy = Strategy().make(
-            name="c51", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "c51", self.capacity, self.train)
         assert isinstance(strategy, c51)
 
     def test_q_learner_agent(self):
         """ Test to see if created agent is q-learner """
 
-        strategy = Strategy().make(
-            name="q-learner", env=self.env, tbl=self.tbl, agent_name="agent"
-        )
+        strategy = Strategy().make(self.env, "q-learner", self.capacity, self.train)
         assert isinstance(strategy, QLearner)
 
-    def test_env(self):
-        """ Test to see if environment is loaded """
-
-        with pytest.raises(TypeError):
-            strategy = Strategy().make(
-                name="q-learner", tbl=self.tbl, agent_name="agent"
-            )
