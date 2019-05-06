201415edf6bd43a5a1ade56b7ca9d2c856b4f689
Tony Hammack <hammack.tony@gmail.com>
2019-05-06 17:51:29 -0500

    Adjust state space

--- rl/c51.py
+++ rl/c51.py
@@ -28,7 +28,7 @@ class c51(Agent):
 
         # Get size of state and action
         # State grows by the amount of frames we want to hold in our memory
-        self.state_size = (3, capacity, 2)
+        self.state_size = (2, capacity, 2)
         self.action_size = 4
 
         # These are the hyper parameters for the c51
