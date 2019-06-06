87459643f27da8cae4a07399008b34af5f417016
Tony Hammack <hammack.tony@gmail.com>
2019-06-06 08:12:20 -0500

    Expand test

--- tests/test_get_config_strategies.py
+++ tests/test_get_config_strategies.py
@@ -7,9 +7,25 @@ class TestStrategyConfig:
     def setup(self):
         pass
 
-    def test_rl_strategy(self):
-        """ Test a rl strategy """
+    def test_c51_strategy(self):
+        """ Test c51 strategy """
         assert isinstance(get_config_strategy("c51"), dict)
+
+    def test_dueling_strategy(self):
+        """ Test dueling strategy """
+        assert isinstance(get_config_strategy("dueling"), dict)
+
+    def test_ddqn_strategy(self):
+        """ Test ddqn strategy """
+        assert isinstance(get_config_strategy("ddqn"), dict)
+
+    def test_qlearner_strategy(self):
+        """ Test qlearner strategy """
+        assert isinstance(get_config_strategy("q-learner"), dict)
+
+    def test_a2c_strategy(self):
+        """ Test a2c strategy """
+        assert isinstance(get_config_strategy("a2c"), dict)
 
     def test_undefined_strategy(self):
         """ Test a strategy not defined """
