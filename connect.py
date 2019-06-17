21341be28f5611961dc8a60f4105700325d90d69
Tony Hammack <hammack.tony@gmail.com>
2019-06-17 08:22:00 -0500

    Massive clean up and pubsub

--- connect.py
+++ connect.py
@@ -24,9 +24,7 @@ class BaseConnection(ABC):
         if isinstance(data, dict):
             for key, value in data.items():
                 try:
-                    if isinstance(value, list) and any(
-                        [item.isdigit() for item in value]
-                    ):
+                    if isinstance(value, list) and any([item.isdigit() for item in value]):
                         data[key] = list(map(int, value))
                 except AttributeError:
                     continue
@@ -49,11 +47,9 @@ class BaseConnection(ABC):
 
 
 class RedisConnection(BaseConnection):
-
-    redis = StrictRedis()
-
     def __init__(self):
-        pass
+        self.redis = StrictRedis()
+        self.p = self.redis.pubsub(ignore_subscribe_messages=True)
 
     def get(self, key: Union[str, List[str]] = ""):
         """ Grab object(s) from Redis and serialize """
@@ -105,75 +101,5 @@ class RedisConnection(BaseConnection):
         for key, value in payload.items():
             self.redis.set(key, self.serialize(value))
 
-
-class ServerConnection(BaseConnection):
-    def __init__(self, test: bool = False):
-
-        url = "http://localhost:8000/api/environment"
-        test_url = "http://localhost:8000/api/test/environment"
-
-        if not test:
-            self.endpoint = url
-        else:
-            self.endpoint = test_url
-
-    def get(self, key: Union[str, List[str]] = ""):
-        """ Grab object(s) from server """
-
-        # If key is not passed, retrieve everything from serevr
-        if not key:
-            r = requests.get(self.endpoint)
-            if r.status_code >= 400:
-                return ""
-            return r.json()
-
-        # If we search based on a single key
-        if key and isinstance(key, str):
-            r = requests.get(self.endpoint, params={"key": key})
-            if r.status_code >= 400:
-                print(f"Object {key} not found")
-                return ""
-            return r.json()
-
-        # If we search based on a multiple keys
-        if key and isinstance(key, list):
-            data = dict()
-            for item in key:
-                r = requests.get(self.endpoint, params={"key": item})
-                if r.status_code >= 400:
-                    print(f"Object {item} not found")
-                    continue
-                data = {**data, **r.json()}
-            return data
-
-        return ""
-
-    def post(self, payload: Any):
-        """ Save data to Server """
-        r = requests.post(self.endpoint, data=payload)
-        if r.status_code >= 400:
-            print(f"Error")
-            return False
-        return True
-
-
-class Connection:
-
-    """ Connection facade
-        We could connect to either redis or local server.
-    """
-
-    connections = {"redis": RedisConnection, "server": ServerConnection}
-
-    def __init__(self):
-        pass
-
-    def make(self, connection: str, test: bool = False):
-
-        if connection == "redis":
-            return self.connections["redis"]()
-
-        if connection == "server":
-            return self.connections["server"](test)
-
-        raise TypeError("Connection not found")
+    def publish(self, channel):
+        self.redis.publish("update", self.serialize(True))
