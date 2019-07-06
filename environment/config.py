fed5339720479235f5e4203416f2dff9e3abb16f
Tony Hammack <hammack.tony@gmail.com>
2019-07-06 16:41:38 -0500

    Can simplify because no more pygame gui

--- environment/config.py
+++ environment/config.py
@@ -3,9 +3,6 @@
 # Table config
 table = {"size": (900, 480), "width_offset": (27, 40), "x_offset": 38}
 
-# Goal config
-goal = {"w": 0, "h": 190}
-
 # Mallet config
 mallet = {"u_lim": 20, "b_lim": 460, "mass": 50, "radius": 20}
 
--- environment/goal.py
+++ environment/goal.py
@@ -1,7 +1,4 @@
 """ Goal Component """
-from typing import Tuple
-
-from environment import config
 
 
 class Goal:
@@ -9,8 +6,5 @@ class Goal:
         """ Create goal """
 
         # Define center of goal
-        self.left_corner_x, self.left_corner_y = x, y
-        self.w, self.h = config.goal["w"], config.goal["h"]
-
-        self.x = int(self.left_corner_x + self.w / 2)
-        self.y = int(self.left_corner_y + self.h / 2)
+        self.x = x
+        self.y = y
